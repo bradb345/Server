6c39ed0f4b5cbafd0dcc900c7eae5dd4aea713f5
bradb345 <bradbernard17@gmail.com>
2021-06-14 20:50:27 +0100

    updating stuff

--- project_expo/views.py
+++ project_expo/views.py
@@ -1,12 +1,11 @@
-# from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Project, Comment
-from .serializers import ProjectSerializer, PopulatedProjectSerializer, PopulatedCommentSerializer
+from .serializers import ProjectSerializer, PopulatedProjectSerializer, CommentSerializer
 
 class ProjectListView(APIView):
 
@@ -25,29 +24,48 @@ class ProjectListView(APIView):
 
 class ProjectDetailView(APIView):
 
-    def get_artist(self, pk):
+    def get_project(self, pk):
         try :
             return Project.objects.get(pk=pk)
         except Project.DoesNotExist:
-            raise NotFound()   
+            raise NotFound()
 
     def get(self, _request, pk):
         try :
-            project = Project.objects.get(pk=pk) # get a artist by id (pk means primary key)
+            project = Project.objects.get(pk=pk) 
             serialized_project = PopulatedProjectSerializer(project)
-            return Response(serialized_project.data,status=status.HTTP_200_OK) # send the JSON to the client 
+            return Response(serialized_project.data,status=status.HTTP_200_OK) 
         except Project.DoesNotExist:
-            raise NotFound()  
+            raise NotFound()
+
+    def delete(self, request, pk):
+        project_to_delete = self.get_project(pk=pk)
+        if project_to_delete.owner != request.user:
+            raise PermissionDenied()
+        project_to_delete.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class CommentListView(APIView):
 
-        permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated, )
 
-        def post(self, request, project_pk):
-            request.data['project'] = project_pk
-            request.data['owner'] = request.user.id
-            serialized_comment = PopulatedCommentSerializer(data=request.data)
-            if serialized_comment.is_valid():
-                serialized_comment.save()
-                return Response(serialized_comment.data, status=status.HTTP_201_CREATED)
-            return Response(serialized_comment.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)    
+    def post(self, request, project_pk):
+        request.data['project'] = project_pk
+        request.data['owner'] = request.user.id
+        serialized_comment = CommentSerializer(data=request.data)
+        if serialized_comment.is_valid():
+            serialized_comment.save()
+            return Response(serialized_comment.data, status=status.HTTP_201_CREATED)
+        return Response(serialized_comment.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+class CommentDetailView(APIView):
+
+    def delete(self, request, _project_pk, comment_pk):
+        try:
+            comment_to_delete = Comment.objects.get(pk=comment_pk)
+            if comment_to_delete.owner != request.user:
+                raise PermissionDenied()
+            comment_to_delete.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Comment.DoesNotExist:
+            raise NotFound()
