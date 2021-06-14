c00ee49bdc1ddb988b997d22679a112e2b745971
Craigc <craigclement@hotmail.com>
2021-06-14 13:26:21 +0100

    paths/views

--- project_expo/views.py
+++ project_expo/views.py
@@ -1,7 +1,9 @@
 # from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 
 from .models import Project
 from .serializers import ProjectSerializer
@@ -13,4 +15,18 @@ class ProjectListView(APIView):
         serialized_project = ProjectSerializer(project, many=True)
         return Response(serialized_project.data, status=status.HTTP_200_OK)
 
-    
+class ProjectDetailView(APIView):
+
+    def get_artist(self, pk):
+        try :
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            raise NotFound()   
+
+    def get(self, _request, pk):
+        try :
+            project = Project.objects.get(pk=pk) # get a artist by id (pk means primary key)
+            serialized_project = ProjectSerializer(project)
+            return Response(serialized_project.data,status=status.HTTP_200_OK) # send the JSON to the client 
+        except Project.DoesNotExist:
+            raise NotFound()
