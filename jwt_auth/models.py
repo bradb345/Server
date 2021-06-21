6ca9e3f55a6c2a8973e8f21ccbff2d5d07f8f006
“gursham” <Gursham2001@hotmail.com>
2021-06-21 16:30:46 +0100

    bug fix

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.CharField(max_length=50)
     profile_image = models.CharField(max_length=250, default='https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png')
-    job_title = models.CharField(max_length=100)
+    job_title = models.CharField(max_length=100, blank=True)
     gacohort = models.CharField(max_length=50, blank=True)
     linkedin = models.CharField(max_length=250, blank=True)
     github = models.CharField(max_length=250, blank=True)
